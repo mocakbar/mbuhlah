aef0242f512442ea1c6e4d6365373d86b6939f3c
brindel <akarmoc@gmail.com>
2020-10-01 11:27:05 +0700

    Updates

---
+++ precache-manifest.0c189bb6c383fd0fbcedd34dd84b19a3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d27665555233e651d848af538d7a0f9",
+    "url": "/mbuhlah/index.html"
+  },
+  {
+    "revision": "d28e3d53e9218adf4f2f",
+    "url": "/mbuhlah/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "531c0f0a82af0b7e2623",
+    "url": "/mbuhlah/static/js/2.3538a92f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mbuhlah/static/js/2.3538a92f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d28e3d53e9218adf4f2f",
+    "url": "/mbuhlah/static/js/main.f6c1a11c.chunk.js"
+  },
+  {
+    "revision": "49dc4a62a02a8f9d4dda",
+    "url": "/mbuhlah/static/js/runtime-main.d64fb604.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mbuhlah/static/media/logo.5d5d9eef.svg"
+  }
+]);
